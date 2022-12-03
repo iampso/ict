3c8d1028aea5736ea217d184df0c482854cd1c2f
iampso <116097799+iampso@users.noreply.github.com>
2022-12-03 20:06:48 +0500

    Create lab6_3_3.java

---
+++ lab6_3_3.java
@@ -0,0 +1,16 @@
+import static java.lang.Math.*;
+
+public class lab6_3_3 {
+    public static void main(String[] args) {
+        double sum = 0;
+
+        for (int i = 1; i <= 8; i++){
+            for (int j = 1; j <= i; j++){
+                for (int k = 1; k <= 3; k++){
+                    sum += pow(j, 2) + i * k;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
