f392e8199d58c5aef47b5459af471730fa4d1711
iampso <116097799+iampso@users.noreply.github.com>
2022-12-03 20:05:21 +0500

    Create lab6_3_1.java

---
+++ lab6_3_1.java
@@ -0,0 +1,15 @@
+import static java.lang.Math.*;
+
+public class lab6_3_1 {
+    public static void main(String[] args) {
+        double sum1 = 0;
+
+        for (int i = 1; i < 9; i++){
+            for (int j = 1; j <= i; j++){
+                sum1 += pow(j, 2);
+            }
+        }
+        System.out.println(sum1);
+
+    }
+}
