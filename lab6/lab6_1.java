d00eaf5cbf20ee2315cfd73273a37c91c62bbbec
iampso <116097799+iampso@users.noreply.github.com>
2023-03-08 21:51:12 +0500

    Add files via upload

---
+++ lab6/lab6_1.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class lab6_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число М");
+        double m = in.nextDouble();
+        System.out.println("Введите кол-во членов ряда");
+        double n = in.nextDouble();
+        double s;
+        System.out.println("Введите х");
+        double x = in.nextDouble();
+//        while ()
+
+    }
+}
+//    public static double Searching(){
+//        double s;
+//        double i = 2;
+//        double x;
+//        s = 1 + 1/pow(x, i)
+//    }
+
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.println("Кол-во членов"); // n- кол-во членов
+//        int n = in.nextInt();
+//
+//        System.out.println("число x"); // Число x
+//        double x = in.nextDouble();
+//
+//        System.out.println("M="); // число
+//        int m = in.nextInt();
+//
+//        int i = 0;
+//        int j = 0;
+//        double S = 1;
+//        double S2 = 0;
+//        while (i < n) {
+//            j += 2;
+//    //              int[] arr = [];
+//
+//            S += 1 / (pow(x, j));
+//            i++;
+//        }
+//        System.out.println("Первый ряд чисел " + S);
+//
+//        while (i < m) {
+//            S2 += 1 / (pow(x, j));
+//            i++;
+//            if (S2 > m) {
+//                break;
+//            }
+//        }
+//    }
+//}
