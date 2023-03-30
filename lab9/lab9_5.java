216a068eaf113c16d642789527cfeef7036fe6e1
roman <116097799+iampso@users.noreply.github.com>
2023-03-30 13:07:32 +0500

    Create lab9_5.java

---
+++ lab9/lab9_5.java
@@ -0,0 +1,21 @@
+package com.test;
+import java.util.Scanner;
+
+public class lab9_5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String text = in.nextLine();
+        String[] words = text.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            if (word.matches("[a-zA-Zа-яА-Я]+")) {
+                result.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1)).append(" ");
+            }
+            else {
+                result.append(word).append(" ");
+            }
+        }
+        System.out.println(result.toString().trim());
+    }
+}
