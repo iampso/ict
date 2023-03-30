755b4ac7d740d01a0eb4b458f9f9cd2c5d3afa77
roman <116097799+iampso@users.noreply.github.com>
2023-03-30 12:06:37 +0500

    Create lab9_1.java

---
+++ lab9/lab9_1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class lab9_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[] arr = new char[10];
+        System.out.println("Введите массив символов из 10 элементов:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.next().charAt(0);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (Character.isDigit(arr[i])) {
+                arr[i] = '*';
+            }
+        }
+        System.out.println("Массив после замены символов-цифр на символ *:");
+        for (char c : arr) {
+            System.out.print(c + " ");
+        }
+    }
+}
