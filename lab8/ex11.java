bda0d663039aab2ea3cc9fbd75146f6aa9d8fd75
roman <116097799+iampso@users.noreply.github.com>
2023-04-23 22:06:05 +0500

    Update ex11.java

--- lab8/ex11.java
+++ lab8/ex11.java
@@ -1,72 +1,57 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class lab8_11 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[][] vertices = new int[3][n*3];
-        int[][] answers = new int[3][n];
-        for (int i = 0; i < n*3; i+=3) {
-            vertices[0][i] = scanner.nextInt();
-            vertices[1][i] = scanner.nextInt();
-            vertices[2][i] = 1;
-            vertices[0][i+1] = scanner.nextInt();
-            vertices[1][i+1] = scanner.nextInt();
-            vertices[2][i+1] = 1;
-            vertices[0][i+2] = scanner.nextInt();
-            vertices[1][i+2] = scanner.nextInt();
-            vertices[2][i+2] = 1;
-        }
-        int count = 0;
-        for (int i = 0; i < n*3; i+=3) {
-            int x1 = vertices[0][i];
-            int y1 = vertices[1][i];
-            int x2 = vertices[0][i+1];
-            int y2 = vertices[1][i+1];
-            int x3 = vertices[0][i+2];
-            int y3 = vertices[1][i+2];
-            double a = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-            double b = Math.sqrt(Math.pow(x3-x2, 2) + Math.pow(y3-y2, 2));
-            double c = Math.sqrt(Math.pow(x1-x3, 2) + Math.pow(y1-y3, 2));
-            if (a+b>c && b+c>a && c+a>b) {
-                answers[0][count] = 1;
-                if (a==b && b==c) {
-                    answers[1][count] = 1;
-                } else if (a==b || b==c || c==a) {
-                    answers[1][count] = 3;
-                } else if (Math.pow(a, 2)+Math.pow(b, 2)==Math.pow(c, 2) || Math.pow(b, 2)+Math.pow(c, 2)==Math.pow(a, 2) || Math.pow(c, 2)+Math.pow(a, 2)==Math.pow(b, 2)) {
-                    answers[1][count] = 2;
-                } else {
-                    answers[1][count] = 4;
-                }
-                double p = (a+b+c)/2;
-                double area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
-                answers[2][count] = (int)area;
+import static java.lang.Math.*;
+
+public class oliver1 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("input n");
+        int n = in.nextInt();
+        int [][] arr = new int[2][3 * n];
+        getArr(arr);
+        double s = 0; double ab; double bc; double ca;
+        double [][] ans = new double[3][n];
+        for (int j = 0, i = 0; j < 3 * n; j += 3, i++){
+            ab = sqrt(pow(arr[0][j] - arr[0][j + 1], 2) + pow(arr[1][j] - arr[1][j + 1], 2));
+            bc = sqrt(pow(arr[0][j + 1] - arr[0][j + 2], 2) + pow(arr[1][j + 1] - arr[1][j + 2], 2));
+            ca = sqrt(pow(arr[0][j + 2] - arr[0][j], 2) + pow(arr[1][j + 2] - arr[1][j], 2));
+            s = 0.5 * abs((arr[0][j] - arr[0][j + 2]) * (arr[1][j + 1] - arr[1][j + 2]) - (arr[0][j + 1] - arr[0][j + 2]) * (arr[1][j] - arr[1][j + 2]));
+            if (s == 0){
+                ans[0][i] = 0;
+                ans[1][i] = 0;
             }
-            count++;
+            else {
+                ans[0][i] = 1;
+                ans[2][i] = s;
+            }
+            if ((ab == bc & bc == ca) & s != 0) ans[1][i] = 1;
+            else if ((ab == bc || bc == ca || ca == ab) & s != 0) ans[1][i] = 3;
+            else if ((pow(ab,2) == pow(bc,2) + pow(ca, 2) || pow(bc,2) == pow(ab,2) + pow(ca, 2) || pow(ca,2) == pow(bc,2) + pow(ab, 2)) & s!= 0) ans[1][i] = 2;
+            else if (s != 0) ans[1][i] = 4;
+
         }
-        printAnswers(answers);
-        checkSameArea(answers);
-    }
-    
-    public static void printAnswers(int[][] answers) {
-        System.out.println("Results:");
-        for (int i = 0; i < answers[0].length; i++) {
-            System.out.println("Triangle " + (i+1) + ":");
-            System.out.println("Existence: " + answers[0][i]);
-            System.out.println("Classification: " + answers[1][i]);
-            System.out.println("Area: " + answers[2][i]);
+        PrintOut(ans);
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (ans[2][i] == ans[2][j] & i != j) System.out.println("area of triangle " + (i + 1) + " equals to triangle " + (j + 1));
+            }
         }
     }
-    
-    public static void checkSameArea(int[][] answers) {
-        System.out.print("Triangles with same area: ");
-        for (int i = 0; i < answers[0].length-1; i++) {
-            for (int j = i+1; j < answers[0].length; j++) {
-                if (answers[0][i]==1 && answers[0][j]==1 && answers[2][i]==answers[2][j]) {
-                    System.out.print((i+1) + ", " + (j+1) + " ");
-                }
+    public static void getArr(int [][] arr){
+        Random r = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = r.nextInt(10);
             }
+        }
+    }
+    public static void PrintOut(double[][] arr){
+        for (double[] ints : arr) {
+            for (double anInt : ints) {
+                System.out.print((int)anInt + " | \t");
+            }
+            System.out.println();
         }
     }
 }
