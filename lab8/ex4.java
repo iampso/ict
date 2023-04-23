e0495e4cdcc7e2abe4223fd78693b4474e2d4425
roman <116097799+iampso@users.noreply.github.com>
2023-04-23 22:07:54 +0500

    Update ex4.java

--- lab8/ex4.java
+++ lab8/ex4.java
@@ -1,75 +1,46 @@
-/* Не тестил */
 import java.util.Scanner;
-import java.util.Random;
 
 public class lab8_4 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Input size of array: ");
-        int n = in.nextInt();
-        int[] arr = new int[n];
-        getArr(arr);
-        ShakerSort(arr);
-        BubbleSort(arr);
-    }
-
-    public static void ShakerSort(int[] arr) {
-        int left = 0;
-        int right = arr.length - 1;
-        int counter = 0;
-
-        while (left <= right) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите кол-во эл-тов массива: ");
+        int n = scan.nextInt(), count = 0;
+        int[] mas = new int[n];
+        System.out.println("Введите эл-ты массива: ");
+        for (int i = 0; i < n; i++) {
+            mas[i] = scan.nextInt();
+        }
+        System.out.println("Исходный массив:");
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(mas[i] + " ");
+        }
+        int left = 0, right = mas.length - 1;
+        do {
+            count++;
             for (int i = left; i < right; i++) {
-                counter++;
-                if (arr[i] >= arr[i + 1]) {
-                    getSwap(arr, i, i + 1);
+                if (mas[i] > mas[i + 1]) {
+                    int in = mas[i];
+                    mas[i] = mas[i + 1];
+                    mas[i + 1] = in;
                 }
-                right--;
             }
+            right--;
             for (int i = right; i > left; i--) {
-                counter++;
-                if (arr[i - 1] >= arr[i]) {
-                    getSwap(arr, i - 1, i);
-                }
-                left++;
-            }
-            if (counter == 0) {
-                System.out.println("Конец, каунтер равен 0");
-                break;
-            }
-        }
-        System.out.println("Количество итераций: " + counter);
-    }
-
-    public static void getArr(int[] arr) {
-        Random r = new Random();
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = r.nextInt(10);
-        }
-    }
-
-    public static void getSwap(int[] arr, int i, int j) {
-        int array = arr[i];
-        arr[i] = arr[j];
-        arr[j] = array;
-    }
-
-    public static void BubbleSort(int[] arr) {
-        int counter = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    counter++;
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                if (mas[i - 1] > mas[i]) {
+                    int in = mas[i];
+                    mas[i] = mas[i - 1];
+                    mas[i - 1] = in;
                 }
             }
-            if (counter == 0) {
-                System.out.println("Конец, каунтер равен 0");
-                break;
-            }
+            left++;
         }
-        System.out.println("Количество итераций: " + counter);
+        while (left <= right);
+        System.out.println();
+        System.out.println("Отсортированный массив: ");
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(mas[i] + " ");
+            System.out.println();
+            System.out.print("Кол-во итерации = " + count);
+        }
     }
 }
